8b3deab0dd17568750110bbfa0626da84f8fb668
Gowtham G <gowthamguruvayurappan29@gmail.com>
2025-09-13 17:53:09 +0530

    Padding adjusted

--- src/Pages/TopSection.jsx
+++ src/Pages/TopSection.jsx
@@ -1,9 +1,10 @@
 import Filters from "../Components/Filters"
+import NavBar from "../Components/NavBar"
 
 function TopSection() {
     return (
-        <div>
-            {/* <NavBar/> */}
+        <div className="flex flex-col gap-5 mt-10 items-center justify-center shadow-lg">
+            <NavBar/>
             <Filters/>
         </div>
     )
