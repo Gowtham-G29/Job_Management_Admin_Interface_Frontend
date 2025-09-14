358b2bb908387cb0b1188004bf3936fbb7e1e6d4
Gowtham G <gowthamguruvayurappan29@gmail.com>
2025-09-15 00:26:26 +0530

    Adjust spacing between navbar and filter component

--- src/Pages/TopSection.jsx
+++ src/Pages/TopSection.jsx
@@ -3,7 +3,7 @@ import NavBar from "../Components/NavBar"
 
 function TopSection() {
     return (
-        <div className="flex flex-col gap-5 mt-10 items-center justify-center shadow-lg">
+        <div className="flex flex-col gap-3 mt-5 items-center justify-center shadow-lg">
             <NavBar/>
             <Filters/>
         </div>
