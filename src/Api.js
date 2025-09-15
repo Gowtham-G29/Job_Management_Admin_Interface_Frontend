32e157b8821e4585d959f075983d222f81da9f31
Gowtham G <gowthamguruvayurappan29@gmail.com>
2025-09-15 16:37:40 +0530

    Api fetched from backend

--- src/Api.js
+++ src/Api.js
@@ -3,7 +3,9 @@ import axios from "axios";
 const BASE_URL = "http://localhost:8080/api/jobs/admin/";
 
 
+
 export const createNewJobPost = async (jobData) => {
+  console.log("Creating job post with data:", jobData);
   try {
     const response = await axios.post(`${BASE_URL}create`, jobData);
     console.log("Job post created successfully:", response.data);
@@ -17,10 +19,22 @@ export const createNewJobPost = async (jobData) => {
 
 export const fetchAllJobPosts = async () => {
   try {
-    const response = await axios.get(`${BASE_URL}getAllJobs`);
+    const response = await axios.get(`${BASE_URL}getAllPosts`);
     return response.data;
   } catch (error) {
     console.error("Error fetching job posts:", error);
     throw error;
   }
 };
+
+export const fetchFilteredJobPosts = async (filters) => {
+  try {
+    const response = await axios.post(`${BASE_URL}getPostsByFilters`, filters);
+    console.log(response);
+    console.log("Filtered job posts:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching filtered job posts:", error);
+    throw error;
+  }
+}
